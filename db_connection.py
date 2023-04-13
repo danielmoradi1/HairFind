608b96d77283f5f103dbf7679172a2990a02ba7f
Nadir <nadirali342@gmail.com>
2023-04-13 16:00:58 +0200

    register users, get data from table
    a function to register a user into database
    a function to read data from table user_table
    a function to read data from table salon_user

--- db_connection.py
+++ db_connection.py
@@ -57,4 +57,80 @@ def register_salon(org_number, name, email, telephone, address, password):
         if cur is not None:
             cur.close()
 
-register_salon(123456, "John Doe", "johndoe@example.com", "123-456-7890", "123 Main St", "password123")
+#register_salon(123456, "John Doe", "johndoe@example.com", "123-456-7890", "123 Main St", "password123")
+
+#Reads data from the table salon_user in database
+def display_table_data(salon_user):
+    conn =None
+    cur = None
+    try: 
+        conn = database_connection()
+        cur = conn.cursor()
+        cur.execute ("SELECT * FROM salon_user")
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
+    except  (Exception, psycopg2.databaseError) as error:
+        print("Error while fetching data from the table salon_user", error)
+    finally: 
+        if cur is not None:
+            cur.close()
+        if conn is not None:
+            conn.close()
+            print("Database connection is closed!")
+
+display_table_data ("salon_user")
+
+#Registers users into the database table "user_table"
+def register_user(userId, firstName, lastName, telephone, emailId, password):
+    conn = None
+    cur = None
+    
+    try:
+        # Read database configuration
+        #connection, cursor = database_connection()
+        conn = database_connection()
+        cur = conn.cursor()
+        
+        insert_script = "INSERT INTO user_table (user_id, First_name, Last_name, telephone, Email_id, password) VALUES(%s, %s, %s, %s, %s, %s)"
+        insert_value =  (userId, firstName, lastName, telephone, emailId, password)
+        cur.execute(insert_script,insert_value)
+
+        # Commit the changes to the database
+        conn.commit()
+        print("User inserted into the table")
+    except (Exception, psycopg2.DatabaseError) as error:
+        print("Error while inserting data in the table", error)
+    finally:
+        if conn is not None:
+            conn.close()
+        if cur is not None:
+            cur.close()
+
+#register_user(145, "Alex","Dahlberg", "07294556621", "johndoe@example.se", "Alex223")
+
+
+#Reads data from the table user_table in database
+def display_table_data(user_table):
+    conn =None
+    cur = None
+    try: 
+        conn = database_connection()
+        cur = conn.cursor()
+        cur.execute ("SELECT * FROM user_table")
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
+    except  (Exception, psycopg2.databaseError) as error:
+        print("Error while fetching data from user_table", error)
+    finally: 
+        if cur is not None:
+            cur.close()
+        if conn is not None:
+            conn.close()
+            print("Database connection is closed!")
+
+display_table_data ("user_table")
+
+
+
