d1de1544d78b2037c3679d3adfa6ecfc1df300fa
Daniel Moradi <Daniel.moradi15@gmail.com>
2023-05-25 23:46:44 +0200

    Confirm delete

---
+++ static/js/delete_confirm.js
@@ -0,0 +1,41 @@
+$(document).ready(function () {
+    $('#deleteBtn').click(function () {
+        Swal.fire({
+            title: 'Ta bort bekräftelse',
+            text: 'Är du säker på att du vill ta bort ditt konto?',
+            icon: 'question',
+            showCancelButton: true,
+            confirmButtonText: 'Ja',
+            cancelButtonText: 'Avbryt'
+        }).then(function (result) {
+            if (result.isConfirmed) {
+                deleteAccount();
+            }
+        });
+    });
+
+    function deleteAccount() {
+        var username = $('#deleteBtn').data('username');
+        $.ajax({
+            url: '/delete_user/' + username,
+            type: 'POST',
+            success: function (response) {
+                if (response.status === 'success') {
+                    Swal.fire({
+                        title: 'Kontot raderat',
+                        text: 'Ditt konto har raderats!',
+                        icon: 'success'
+                    }).then(function () {
+                        window.location.href = response.redirect;
+                    });
+                } else {
+                    Swal.fire({
+                        title: 'Error',
+                        text: response.message,
+                        icon: 'error'
+                    });
+                }
+            }
+        });
+    }
+});
